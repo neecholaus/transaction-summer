57e2bf558da7eded4d15af0d6d64810ba51f58b9
nick <nick@trendcapital.com>
2022-07-07 13:53:34 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module transaction-summer
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	sources := transactionSources()
+
+	dict := make(map[string]float64)
+
+	for _, v := range sources {
+		processSource(v, &dict)
+	}
+
+	for k, v := range dict {
+		fmt.Println(k, v)
+	}
+}
+
+func transactionSources() []string {
+	files, err := ioutil.ReadDir("./records")
+	if err != nil {
+		return []string{}
+	}
+
+	var sources []string
+	for _, v := range files {
+		sources = append(sources, v.Name())
+	}
+
+	return sources
+}
+
+func processSource(source string, dict *map[string]float64) {
+	file, err := os.Open("./records/" + source)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		cleaned := line[1 : len(line)-1]
+		split := strings.Split(cleaned, "\",\"")
+
+		category := split[8]
+		amount, _ := strconv.ParseFloat(split[4], 64)
+
+		if category != "" {
+			(*dict)[category] += (amount * -1)
+		} else if amount < 0 {
+			(*dict)["Unknown"] += amount
+		}
+	}
+}
